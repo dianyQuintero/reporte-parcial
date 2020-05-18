ecd5306fa71ed17369c8b023474a6cc76cc9407c
DIANY YISEL QUINTERO PEROZO <dy.quintero@uniandes.edu.co>
2020-05-17 19:39:09 -0500

    Arreglos

--- src/Components/performance.js
+++ src/Components/performance.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import over1 from '../assets/imgs/Over (1).jpg'
+import over2 from '../assets/imgs/Over (2).jpg'
+import over3 from '../assets/imgs/Over (3).jpg'
+import over4 from '../assets/imgs/Over (4).jpg'
+import over5 from '../assets/imgs/Over (5).jpg'
+import over6 from '../assets/imgs/Over (6).jpg'
+
 
 
 
@@ -41,13 +48,55 @@ export default class Performance extends React.Component{
                         <div className="col-12">
                             <ul>
                                 <li>
-                                    <h5 align="justify" > Encontramos que se hace bsatente uso de los findViewById(),
-                                    en una de las clases principales VideoPlayerActivity.kt se repite 23 veces y teniendo
-                                    en cuenta lo que vimos en clase recomendaríamos que se cambiaran por 
+                                    <h5 align="justify" > Se utiliza en repetidas ocasiones FindViewById() 
+                                    (23 veces en una de las clases principales, VideoPlayerActivity.kt), lo cual
+                                    puede afectar negativamente el performance de aplicación. Sería recomendable 
+                                    no hacer tanto uso de éste método, una posible solución es usar binding.
+                                    </h5>
+                                </li>
+                                <li>
+                                    <h5 align="justify" > Al realizar un GPU overdraw descubrimos que hay problemas con 
+                                    la barra de navegación inferior y con el fragmento en que se muestran todos videos
+                                    guardados localmente. Esto puede deberse a que no se están usando componentes con el 
+                                    fondo transparente. 
                                     </h5>
                                 </li>
                             </ul>
                             </div>
+                            <div className="col-12">
+                            <div className="card  py-2"  >
+                                <div className="card-body">
+                                <div className="row">
+                                    <div className="col-4">
+                                    <img src={over1} width="300" height="600" style={{"display": "block", "margin-left": "auto", "margin-right": "auto" }}  className="img-fluid" alt="versionesStyles"/>   
+                                    </div>
+                                    <div className="col-4">
+                                    <img src={over2} width="300" height="600" style={{"display": "block", "margin-left": "auto", "margin-right": "auto" }}  className="img-fluid" alt="versionesStyles"/>   
+                                   </div>
+                                   <div className="col-4">
+                                    <img src={over3} width="300" height="600" style={{"display": "block", "margin-left": "auto", "margin-right": "auto" }}  className="img-fluid" alt="versionesStyles"/>   
+                                   </div>
+                                </div>
+                                    </div>
+                            </div>
+                        </div>
+                        <div className="col-12">
+                            <div className="card  py-2"  >
+                                <div className="card-body">
+                                <div className="row">
+                                    <div className="col-4">
+                                    <img src={over4} width="300" height="600" style={{"display": "block", "margin-left": "auto", "margin-right": "auto" }}  className="img-fluid" alt="versionesStyles"/>   
+                                    </div>
+                                    <div className="col-4">
+                                    <img src={over5} width="300" height="600" style={{"display": "block", "margin-left": "auto", "margin-right": "auto" }}  className="img-fluid" alt="versionesStyles"/>   
+                                   </div>
+                                   <div className="col-4">
+                                    <img src={over6} width="300" height="600" style={{"display": "block", "margin-left": "auto", "margin-right": "auto" }}  className="img-fluid" alt="versionesStyles"/>   
+                                   </div>
+                                </div>
+                                    </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
               
